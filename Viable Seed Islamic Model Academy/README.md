80d3024d92aa1b977533406f992bf20529406655
Prof. NovaSpire <umarabiodunluqman@gmail.com>
2026-02-14 10:47:42 +0100

    Delete Viable Seed Islamic Model Academy directory

--- Viable Seed Islamic Model Academy/README.md
+++
@@ -1,2 +0,0 @@
-# Viable-Seeds-Islamic-Model-Academy
-A modern institution that focuses on Islamic, Western and Digital knowldge
--- Viable Seed Islamic Model Academy/config.js
+++
@@ -1,2 +0,0 @@
-const SUPABASE_URL = 'https://chhkghparlgsikxzxasw.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_z0Fv9IFDXOCCdCCUqF6j0w_J0bWFIET';
